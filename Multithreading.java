3d638d75e6579e216f147ecdd045c8b478c25739
Aashalatha P A <93538340+aashalatha@users.noreply.github.com>
2022-07-12 23:27:16 +0530

    Update Multithreading.java

--- Multithreading.java
+++ Multithreading.java
@@ -1,18 +1,23 @@
 import java.util.*;
 
-class Even implements Runnable{
+class Even implements Runnable
+{
 int s, n;
 
-public Even(int s, int n){
+public Even(int s, int n)
+{
 this.s=s;
 this.n=n;
 }
 
-public void run(){
+public void run()
+{
 
-for(int i=s; i<=n; i++){
-if (i%2==0){
-System.out.println(""+i);
+for(int i=s; i<=n; i++)
+{
+if (i%2==0)
+{
+System.out.println(" "+i);
 }
 } 
 
@@ -20,22 +25,27 @@ System.out.println(""+i);
 
 }
 
-class Fibonacci implements Runnable{
+class Fibonacci implements Runnable
+{
 int n;
 
-	public Fibonacci(int n){
+	public Fibonacci(int n)
+	{
 		this.n=n;
 	}
 
-	public void run(){
+	public void run()
+	{
 		int a,b,c,i;
 		a=c=i=0;
 		b=1;
 
 
-		while(i++<n){
-			System.out.println(i+"th" +" Fib no: = "+a);
+		System.out.print(a+" "+b);
+		for(i=0;i<n-2;i++)
+		{
 			c=a+b;
+			System.out.print(" "+c);
 			a=b;
 			b=c;
 		}
@@ -43,22 +53,24 @@ int n;
 	}
 }
 
-class Multithreading{
+class Multithreading
+{
 
 public static void main(String args[])
 { 
 Scanner sc = new Scanner(System.in);
 
-System.out.println("Enter range:");
-System.out.println("Starting: ");
-int s=sc.nextInt();
-System.out.println("End: ");
+System.out.println("Enter the limit for fibonacci series:");
 int n=sc.nextInt();
-
 Thread t1 = new Thread(new Fibonacci(n));
-Thread t2 = new Thread(new Even(s,n));
+t1.run();
 
-t1.start();
+System.out.println("\nEnter Starting range for even numbers: ");
+int s=sc.nextInt();
+System.out.println("Enter Ending range for even numbers: ");
+n=sc.nextInt();
+System.out.print("The even numbers are: ");
+Thread t2 = new Thread(new Even(s,n));
 t2.start();
 
 }
